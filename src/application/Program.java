bfd7a3c5783764d33e3ca476116e71631cb05ac2
nichmelo <nicholas.paz.melo@gmail.com>
2021-05-25 19:05:49 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-		Department obj = new Department(1, "Games");
-		System.out.println(obj);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
+		
+		Department department = new Department(1, "Games");
+		
+		Seller seller = new Seller(21, "Nicholas", "nicholas@gmail.com", sdf.parse("22/02/1996"), 3500.0, department);
+		
+		System.out.println(seller);
 	}
 
 }
